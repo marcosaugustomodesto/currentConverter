675e92834412dc80bb6e78f57629d9c189a30da6
Marcos <marcosaugustomodesto15@gmail.com>
2024-02-19 20:07:10 -0300

    initial commit

---
+++ src/constants.ts
@@ -0,0 +1,21 @@
+export const currencyByReal: Currency[] = [
+    {
+        name: 'Dolar Americano',
+        value: 0.2,
+        flag:'🇺🇸',
+        symbol:'$'
+    },
+    {
+        name: 'Libra Esterlina',
+        value: 0.15928,
+        flag:'🇬🇧',
+        symbol:'£'
+    },
+    {
+        name: 'Euro',
+        value: 0.19,
+        flag:'🇪🇺',
+        symbol:'€'
+    }
+
+];
